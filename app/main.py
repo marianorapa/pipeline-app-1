29a5986c3843c0367cc7f9aecd6be2460e3f1aed
marianorapa <marianorapaport@gmail.com>
2025-05-08 23:23:59 +0000

    update message

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, World! This is an update that will be deployed in a bit by the CI-CD pipeline (:"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
